8829dd2e06314d2538b476010ddd76b75614db46
WENLIXIAO-CS <randyxiao64@gmail.com>
2025-05-07 21:59:19 -0400

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+export default nextConfig
